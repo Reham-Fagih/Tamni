9a26d6cd74c4f81d08361391ea7c0cfbc6e8dd8a
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2024-11-02 16:58:26 +0300

    Fix:changes to the files name and the .gitignore

---
+++ user.js
@@ -0,0 +1,17 @@
+const User = require('./db');
+const bcrypt = require('bcrypt');
+
+async function createUser(name, password) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ name, password: hashedPassword });
+  return await newUser.save();
+}
+
+async function findUserByName(name) {
+  return await User.findOne({ name });
+}
+
+module.exports = {
+  createUser,
+  findUserByName
+};
