24876252130ae21e520e15eefab5c442d8e5536f
Shatha <2210787@uj.edu.sa>
2024-11-19 01:12:44 +0300

    Modified ChatRoom.html, modified signUp.html, deleted chatRedirect.js

--- public/chatRedirect.js
+++
@@ -1,9 +0,0 @@
-function closeChat() {
-    var userType = document.getElementById("role").value;
-    
-    if (userType === "doctor") {
-        window.location.href = 'doctorpage.html';
-    } else if (userType === "patient") {
-        window.location.href = 'index.html';
-    }
-}
